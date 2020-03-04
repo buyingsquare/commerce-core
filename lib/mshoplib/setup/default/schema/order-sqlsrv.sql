212837d111792cf8ecef79dbd82365e8c196e15b
Aimeos <aimeos@aimeos.org>
2020-03-04 22:39:27 +0100

    Fixed sqlsrv indexes

--- lib/mshoplib/setup/default/schema/order-sqlsrv.sql
+++ lib/mshoplib/setup/default/schema/order-sqlsrv.sql
@@ -2,6 +2,6 @@
 -- SQL Server specific database definitions
 --
 
-CREATE INDEX "idx_msordbaprat_si_cd_va" ON "mshop_order_base_product_attr" ("siteid", "code", "value");
+CREATE INDEX "idx_msordbaprat_si_cd" ON "mshop_order_base_product_attr" ("siteid", "code");
 
-CREATE INDEX "idx_msordbaseat_si_cd_va" ON "mshop_order_base_service_attr" ("siteid", "code", "value");
+CREATE INDEX "idx_msordbaseat_si_cd" ON "mshop_order_base_service_attr" ("siteid", "code");
--- lib/mshoplib/setup/default/schema/text-sqlsrv.sql
+++ lib/mshoplib/setup/default/schema/text-sqlsrv.sql
@@ -2,4 +2,4 @@
 -- SQL Server specific database definitions
 --
 
-CREATE INDEX "idx_mstex_sid_dom_cont" ON "mshop_text" ("siteid", "domain", "content");
+CREATE INDEX "idx_mstex_sid_dom" ON "mshop_text" ("siteid", "domain");
