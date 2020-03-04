53a031fa8ad342d5b34daac322462308f762f370
Aimeos <aimeos@aimeos.org>
2020-03-04 22:39:57 +0100

    Added sqlsrv index text manager

---
+++ lib/mshoplib/src/MShop/Index/Manager/Text/SQLSrv.php
@@ -0,0 +1,101 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2020
+ * @package MShop
+ * @subpackage Index
+ */
+
+
+namespace Aimeos\MShop\Index\Manager\Text;
+
+
+/**
+ * SQL Server based index text for searching in product tables.
+ *
+ * @package MShop
+ * @subpackage Index
+ */
+class SQLSrv
+	extends \Aimeos\MShop\Index\Manager\Text\Standard
+{
+	private $searchConfig = array(
+		'index.text:relevance' => array(
+			'code' => 'index.text:relevance()',
+			'internalcode' => ':site AND mindte."langid" = $1 AND CONTAINS( mindte."content", $2 )',
+			'label' => 'Product texts, parameter(<language ID>,<search term>)',
+			'type' => 'float',
+			'internaltype' => \Aimeos\MW\DB\Statement\Base::PARAM_FLOAT,
+			'public' => false,
+		),
+		'sort:index.text:relevance' => array(
+			'code' => 'sort:index.text:relevance()',
+			'internalcode' => '1',
+			'label' => 'Product text sorting, parameter(<language ID>,<search term>)',
+			'type' => 'float',
+			'internaltype' => \Aimeos\MW\DB\Statement\Base::PARAM_FLOAT,
+			'public' => false,
+		),
+	);
+
+
+	/**
+	 * Initializes the object
+	 *
+	 * @param \Aimeos\MShop\Context\Item\Iface $context Context object
+	 */
+	public function __construct( \Aimeos\MShop\Context\Item\Iface $context )
+	{
+		parent::__construct( $context );
+
+		$level = \Aimeos\MShop\Locale\Manager\Base::SITE_ALL;
+		$level = $context->getConfig()->get( 'mshop/index/manager/sitemode', $level );
+
+		$func = function( $source, array $params ) {
+
+			if( isset( $params[1] ) )
+			{
+				$str = '';
+				$regex = '/(\&|\||\!|\-|\+|\>|\<|\(|\)|\~|\*|\:|\"|\'|\@|\\| )+/';
+				$search = trim( preg_replace( $regex, ' ', $params[1] ), "' \t\n\r\0\x0B" );
+
+				foreach( explode( ' ', $search ) as $part )
+				{
+					$len = strlen( $part );
+
+					if( $len > 0 ) {
+						$str .= ' "' . mb_strtolower( $part ) . '*"';
+					}
+				}
+
+				$params[1] = '\'' . $str . '\'';
+			}
+
+			return $params;
+		};
+
+		$name = 'index.text:relevance';
+		$expr = $this->getSiteString( 'mindte."siteid"', $level );
+		$this->searchConfig[$name]['internalcode'] = str_replace( ':site', $expr, $this->searchConfig[$name]['internalcode'] );
+		$this->searchConfig['index.text:relevance']['function'] = $func;
+	}
+
+
+	/**
+	 * Returns a list of objects describing the available criterias for searching.
+	 *
+	 * @param bool $withsub Return also attributes of sub-managers if true
+	 * @return \Aimeos\MW\Criteria\Attribute\Iface[] List of search attriubte items
+	 */
+	public function getSearchAttributes( bool $withsub = true ) : array
+	{
+		$list = parent::getSearchAttributes( $withsub );
+
+		foreach( $this->searchConfig as $key => $fields ) {
+			$list[$key] = new \Aimeos\MW\Criteria\Attribute\Standard( $fields );
+		}
+
+		return $list;
+	}
+}
