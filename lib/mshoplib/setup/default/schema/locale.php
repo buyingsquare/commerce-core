38262829ccb0e85ba0e404787b5db8a9fda13969
Aimeos <aimeos@aimeos.org>
2020-03-06 10:54:08 +0100

    Use same column definition for siteid

--- lib/mshoplib/setup/default/schema/locale.php
+++ lib/mshoplib/setup/default/schema/locale.php
@@ -77,7 +77,7 @@ return array(
 			$table = $schema->createTable( 'mshop_locale' );
 
 			$table->addColumn( 'id', 'integer', array( 'autoincrement' => true ) );
-			$table->addColumn( 'siteid', 'string', ['length' => 255] );
+			$table->addColumn( 'siteid', 'string', ['length' => 255, 'default' => ''] );
 			$table->addColumn( 'langid', 'string', array( 'length' => 5 ) );
 			$table->addColumn( 'currencyid', 'string', array( 'length' => 3 ) );
 			$table->addColumn( 'pos', 'integer', [] );
